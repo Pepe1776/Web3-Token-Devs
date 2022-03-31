c1cc5f4a97f6b5adde0f9e6cd854fee1739ecc5e
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-03-30 17:10:22 -0700

    initial

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xE5e587af70e4f4Fd5FD5765DFfb6D5AAe776a8B2";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
